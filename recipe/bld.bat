2d71987145046b4226431167acad3be25fe94ade
Talley Lambert <talley.lambert@gmail.com>
2021-05-27 16:22:44 -0400

    add install prefix

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,11 @@
 mkdir cmake_build
 cd cmake_build
-cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release ../src
+
+cmake -G Ninja ^
+    -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+    -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    ../src
+
 ninja
 ninja install
